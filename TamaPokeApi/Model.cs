3039282eb65f78558a4145359edf17d9209fb8b5
Adriano Yudi R. Nishimoto <rnishimotoyudi123@gmail.com>
2025-02-27 10:33:40 -0400

    Corrigido NullReferenceException em Abilities, inicializando listas e tratando null

---
+++ TamaPokeApi/Model.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TamagotchiPokemon {
+    public class Ability {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("is_hidden")]
+        public bool IsHidden { get; set; }
+
+        [JsonPropertyName("slot")]
+        public int Slot { get; set; }
+    }
+
+    public class Pokemon {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("height")]
+        public int Height { get; set; } 
+
+        [JsonPropertyName("weight")]
+        public int Weight { get; set; } 
+
+        [JsonPropertyName("abilities")]
+        public List<Ability> Abilities { get; set; } = new List<Ability>(); 
+    }
+
+    public class PokemonResponse {
+        [JsonPropertyName("results")]
+        public List<Pokemon> Results { get; set; } = new List<Pokemon>(); 
+    }
+}
